1710e3fb5b0af69a0591b09d7a1c7f5b6b5f58b9
Mike <mike.magnussen@lifeway.com>
2019-01-22 14:28:39 -0600

    pangram

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -1,14 +1,14 @@
 const isPangram = (str) => {
-    const cleanPhrase = str.toLowerCase().replace(/[^a-z]/gi, '').split('');
     //replace all non-letters with nothing
     //then use Set to determine only unique letters
     //drop to lowercase
-    //then add ASCI of those remaining letters for lowercase values sum (2847)
+    const cleanPhrase = str.toLowerCase().replace(/[^a-z]/gi, '').split('');
 
-    // if new Set.size !== 26/25 then
+    // if new Set.size !== 26 then
     const letterSet = new Set(cleanPhrase);
 
-    if (letterSet.size = 26) {
+    // Use Set .size property to determine length of the returned letterSet
+    if (letterSet.size == 26) {
         return true
     }
     else {
