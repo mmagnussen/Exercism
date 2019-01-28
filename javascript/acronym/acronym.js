09950e924017786c98bb9d3ca05321397b0a90d2
Mike <mike.magnussen@lifeway.com>
2019-01-28 11:09:37 -0600

    acronym

--- javascript/acronym/acronym.js
+++ javascript/acronym/acronym.js
@@ -1,13 +1,11 @@
 const parse = (phrase) => {
 
-    const strArray = phrase.split(/[ -]/);
-    console.log(strArray);
-    let firstLetters;
-    var i;
-    for (i of strArray) {
-        return firstLetters.push(strArray[i].charAt(0));
-    }
-    const newAccro = newAccro.join(firstLetters).toUpperCase();
+
+    //Split phrase on a regex that includes capture groups (to be retained); 
+    //Then .filter to test for a truthy in that returned group
+    //Use .map to iterate through the array, and remove the first character
+    //Then join the array of first characters, and convert ot upper case
+    return phrase.split(/(?:[a-z]([A-Z][a-z]*)|[ -])/).filter(x => x).map(splitElement => splitElement.slice(0, 1)).join('').toUpperCase()
 }
 
 
--- javascript/acronym/acronym.spec.js
+++ javascript/acronym/acronym.spec.js
@@ -5,23 +5,23 @@ describe('Acronyms are produced from', () => {
     expect(parse('Portable Network Graphics')).toEqual('PNG');
   });
 
-  xtest('other title cased phrases', () => {
+  test('other title cased phrases', () => {
     expect(parse('Ruby on Rails')).toEqual('ROR');
   });
 
-  xtest('inconsistently cased phrases', () => {
+  test('inconsistently cased phrases', () => {
     expect(parse('HyperText Markup Language')).toEqual('HTML');
   });
 
-  xtest('phrases with punctuation', () => {
+  test('phrases with punctuation', () => {
     expect(parse('First In, First Out')).toEqual('FIFO');
   });
 
-  xtest('other phrases with punctuation', () => {
+  test('other phrases with punctuation', () => {
     expect(parse('PHP: Hypertext Preprocessor')).toEqual('PHP');
   });
 
-  xtest('phrases with punctuation and sentence casing', () => {
+  test('phrases with punctuation and sentence casing', () => {
     expect(parse('Complementary metal-oxide semiconductor')).toEqual('CMOS');
   });
 });
