52b287e8843c55e58f992b33f9319033655dbf17
Mike <mike.magnussen@lifeway.com>
2019-01-30 10:59:32 -0600

    bob

--- javascript/acronym/acronym.js
+++ javascript/acronym/acronym.js
@@ -1,12 +1,10 @@
 const parse = (phrase) => {
 
-
     //Split phrase on a regex that includes capture groups (to be retained); 
     //Then .filter to test for a truthy in that returned group
     //Use .map to iterate through the array, and remove the first character
     //Then join the array of first characters, and convert ot upper case
     return phrase.split(/(?:[a-z]([A-Z][a-z]*)|[ -])/).filter(x => x).map(splitElement => splitElement.slice(0, 1)).join('').toUpperCase()
 }
-
 
 export { parse }
--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -2,13 +2,35 @@
 //
 // This is only a SKELETON file for the 'Bob' exercise. It's been provided as a
 // convenience to get you started writing code faster.
-//
 
-export const hey = (message) => {
 
-  const lastChar = (message[message.length - 1]);
+export const hey = message => {
 
-  const response = lastChar == "?" ? "Sure." : (lastChar == "!" ? "Fine. Be that way!" : "Whatever.");
+  //evaluate whether a string has all uppercase letter, but allows non-letter characters
+  const isUpperCase = message.toUpperCase();
+  const lowerCaseLetters = message.match(/[a-z]/i);
+  // Establish the return strings for the five conditions:
+  let responses = {
+    force: 'Calm down, I know what I\'m doing!',
+    question: 'Sure.',
+    yell: 'Whoa, chill out!',
+    nothing: 'Fine. Be that way!',
+    rest: 'Whatever.'
+  }
 
-  return response;
-};
+  return (
+    //First, dealing with strings ending in question marks:
+
+    //Condition for the single occurrence of a string with all caps ending with a question mark
+    (message.slice(-1) === '?' && message === isUpperCase && lowerCaseLetters) ? responses.force
+      //Condition for the other strings that end with a question mark  
+      : (message.trim().slice(-1) === '?') ? responses.question
+        //Condition for other strings that are all caps, with no mixed case
+        : (message === isUpperCase && lowerCaseLetters) ? responses.yell
+          //Condition for only white space with no letters or numbers
+          : (!message.match(/[a-z,0-9]/i)) ? responses.nothing
+            //Condition for the remaining strings
+            : responses.rest
+  )
+}
+
